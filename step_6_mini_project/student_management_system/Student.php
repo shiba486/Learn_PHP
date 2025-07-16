c12bdd31c366d5d889f2875bac6942e6a0509fb8
Shibanondo Das <145425370+shiba486@users.noreply.github.com>
2025-07-16 21:11:07 +0600

    Update Student.php

--- step_6_mini_project/student_management_system/Student.php
+++ step_6_mini_project/student_management_system/Student.php
@@ -1,1 +1,31 @@
+<?php
+require_once 'Person.php';
+
+class Student extends Person {
+    private $roll;
+    private $gpa;
+
+    public function setRoll($roll) {
+        $this->roll = $roll;
+    }
+    public function getRoll() {
+        return $this->roll;
+    }
+
+    public function setGpa($gpa) {
+        if ($gpa >= 0 && $gpa <= 4.0) {
+            $this->gpa = $gpa;
+        }
+    }
+    public function getGpa() {
+        return $this->gpa;
+    }
+
+    public function showStudentInfo() {
+        $this->showBasicInfo();
+        echo "🆔 roll: $this->roll<br>";
+        echo "📊 GPA: $this->gpa<br><br>";
+    }
+}
+?>
 
