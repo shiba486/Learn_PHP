87562ff912c5499c72592ea92da9b47f550871e7
Shibanondo Das <145425370+shiba486@users.noreply.github.com>
2025-07-16 21:10:03 +0600

    Update Person.php

--- step_6_mini_project/student_management_system/Person.php
+++ step_6_mini_project/student_management_system/Person.php
@@ -1,1 +1,28 @@
-<?php echo "hello world";?>
+
+<?php
+class Person {
+    protected $name;
+    protected $email;
+
+    public function setName($name) {
+        $this->name = $name;
+    }
+    public function getName() {
+        return $this->name;
+    }
+
+    public function setEmail($email) {
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $this->email = $email;
+        }
+    }
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function showBasicInfo() {
+        echo "👤 name: $this->name<br>";
+        echo "📧 email: $this->email<br>";
+    }
+}
+?>
